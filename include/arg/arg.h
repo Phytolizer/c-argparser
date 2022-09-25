b3f0c766e40cd12dbbee8ed33558e7c0b930232b
Kyle Coffey <me@phytolizer.dev>
2022-09-24 19:20:43 -0500

    formatting consistency

--- include/arg/arg.h
+++ include/arg/arg.h
@@ -7,43 +7,43 @@
 #include <stdio.h>
 
 typedef enum {
-  ARG_KIND_POS,
-  ARG_KIND_OPT,
-  ARG_KIND_FLAG,
+	ARG_KIND_POS,
+	ARG_KIND_OPT,
+	ARG_KIND_FLAG,
 } ArgKind;
 
 typedef struct {
-  ArgKind kind;
-  char shortname;
-  ArgStr longname;
-  ArgStr help;
-  // set if kind == ARGKIND_OPT or ARGKIND_POS
-  ArgStr value;
-  // set if kind == ARGKIND_FLAG
-  bool flagValue;
+	ArgKind kind;
+	char shortname;
+	ArgStr longname;
+	ArgStr help;
+	// set if kind == ARGKIND_OPT or ARGKIND_POS
+	ArgStr value;
+	// set if kind == ARGKIND_FLAG
+	bool flagValue;
 } Arg;
 
 #define ARG_POS(name, hlp)                                                     \
-  (Arg) { .kind = ARG_KIND_POS, .longname = name, .help = hlp }
+	(Arg) { .kind = ARG_KIND_POS, .longname = name, .help = hlp }
 
 #define ARG_OPT(...)                                                           \
-  (Arg) { .kind = ARG_KIND_OPT, __VA_ARGS__ }
+	(Arg) { .kind = ARG_KIND_OPT, __VA_ARGS__ }
 
 #define ARG_FLAG(...)                                                          \
-  (Arg) { .kind = ARG_KIND_FLAG, __VA_ARGS__ }
+	(Arg) { .kind = ARG_KIND_FLAG, __VA_ARGS__ }
 
-typedef ARG_BUF(Arg *) ArgBuf;
+typedef ARG_BUF(Arg*) ArgBuf;
 typedef ARG_BUF(ArgStr) ArgStrBuf;
 
 typedef struct {
-  ArgBuf args;
-  ArgStr name;
-  ArgStr help;
-  ArgStrBuf extra;
+	ArgBuf args;
+	ArgStr name;
+	ArgStr help;
+	ArgStrBuf extra;
 } ArgParser;
 
 typedef ARG_MAYBE(ArgStr) ArgParseErr;
 
 ArgParser arg_parser_new(ArgStr name, ArgStr help, ArgBuf args);
-ArgParseErr arg_parser_parse(ArgParser *parser, int argc, char **argv);
-void arg_parser_show_help(ArgParser *parser, FILE *fp);
+ArgParseErr arg_parser_parse(ArgParser* parser, int argc, char** argv);
+void arg_parser_show_help(ArgParser* parser, FILE* fp);
--- include/arg/buf.h
+++ include/arg/buf.h
@@ -6,49 +6,49 @@
 #include <stdlib.h>
 
 #define ARG_BUF(T)                                                             \
-  struct {                                                                     \
-    T *ptr;                                                                    \
-    uint64_t len;                                                              \
-    uint64_t cap;                                                              \
-    bool ref;                                                                  \
-  }
+	struct {                                                                     \
+		T *ptr;                                                                    \
+		uint64_t len;                                                              \
+		uint64_t cap;                                                              \
+		bool ref;                                                                  \
+	}
 
 #define ARG_BUF_EMPTY                                                          \
-  { .ptr = NULL, .len = 0, .cap = 0, .ref = false }
+	{ .ptr = NULL, .len = 0, .cap = 0, .ref = false }
 
 #define ARG_BUF_REF(p, sz)                                                     \
-  { .ptr = (p), .len = (sz), .cap = (sz), .ref = true }
+	{ .ptr = (p), .len = (sz), .cap = (sz), .ref = true }
 
 #define ARG_BUF_ARRAY(a) ARG_BUF_REF((a), sizeof(a) / sizeof((a)[0]))
 
 #define ARG_BUF_OWNER(p, sz)                                                   \
-  { .ptr = (p), .len = (sz), .cap = (sz), .ref = false }
+	{ .ptr = (p), .len = (sz), .cap = (sz), .ref = false }
 
 #define ARG_BUF_NEW                                                            \
-  { .ptr = NULL, .len = 0, .cap = 0, .ref = false }
+	{ .ptr = NULL, .len = 0, .cap = 0, .ref = false }
 
 #define ARG_BUF_FREE(buf)                                                      \
-  do {                                                                         \
-    if (!(buf).ref) {                                                          \
-      free((buf).ptr);                                                         \
-    }                                                                          \
-  } while (false)
+	do {                                                                         \
+		if (!(buf).ref) {                                                          \
+			free((buf).ptr);                                                         \
+		}                                                                          \
+	} while (false)
 
 #define ARG_BUF_PUSH(buf, val)                                                 \
-  do {                                                                         \
-    if ((buf)->len == (buf)->cap) {                                            \
-      (buf)->cap = (buf)->cap ? (buf)->cap * 2 : 1;                            \
-      (buf)->ptr = realloc((buf)->ptr, (buf)->cap * sizeof(*(buf)->ptr));      \
-    }                                                                          \
-    (buf)->ptr[(buf)->len++] = (val);                                          \
-  } while (false)
+	do {                                                                         \
+		if ((buf)->len == (buf)->cap) {                                            \
+			(buf)->cap = (buf)->cap ? (buf)->cap * 2 : 1;                            \
+			(buf)->ptr = realloc((buf)->ptr, (buf)->cap * sizeof(*(buf)->ptr));      \
+		}                                                                          \
+		(buf)->ptr[(buf)->len++] = (val);                                          \
+	} while (false)
 
 #define ARG_BUF_FIND(buf, val, eq, target)                                     \
-  do {                                                                         \
-    for (uint64_t i = 0; i < (buf).len; i++) {                                 \
-      if (eq((buf).ptr[i], (val))) {                                           \
-        *(target) = &(buf).ptr[i];                                             \
-        break;                                                                 \
-      }                                                                        \
-    }                                                                          \
-  } while (false)
+	do {                                                                         \
+		for (uint64_t i = 0; i < (buf).len; i++) {                                 \
+			if (eq((buf).ptr[i], (val))) {                                           \
+				*(target) = &(buf).ptr[i];                                             \
+				break;                                                                 \
+			}                                                                        \
+		}                                                                          \
+	} while (false)
